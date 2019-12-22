958883af0c37e53bd7e15c728a42eed53269b1ee
Janne Toivola <jammu81@iki.fi>
2019-12-22 03:06:49 +0200

    2019 day 10 part 2

--- 2019/monitoring_station.rb
+++ 2019/monitoring_station.rb
@@ -43,6 +43,47 @@ class AsteroidMap
     str.string
   end
 
+  # Iterate through asteroids and zap them with lazer
+  # output each of the coordinates of zapped asteroids in order
+  # @param column_index [Integer] coordinate of monitoring station
+  # @param row_index [Integer] coordinate of monitoring station
+  # @return [Array] each of the asteroid coordinates in zapping order
+  def lazer_sweep(column_index, row_index)
+    targets = asteroid_directions(column_index, row_index)
+    coordinates = []
+    until targets.empty?
+      directions = targets.keys.sort
+      directions.each do |direction|
+        min_distance = targets[direction].min_by { |t| t[:distance] }
+        targets[direction].delete(min_distance) # could just pop if sorted
+
+        targets.delete(direction) if targets[direction].empty?
+
+        coordinates << [column_index + min_distance[:x],
+                        row_index + min_distance[:y]]
+      end
+    end
+    coordinates
+  end
+
+  # Find the best asteroid on the map in terms of visible other asteroids
+  # @return [Hash] with [Integer] :count and [Array<Integer>] :coordinates
+  def best_monitoring_asteroid
+    best = {}
+    all = visible_asteroids
+    all.each.with_index do |row, y|
+      row.each.with_index do |count, x|
+        next if count.nil?
+
+        if best[:count].nil? || best[:count] < count
+          best[:count] = count
+          best[:coordinates] = [x, y]
+        end
+      end
+    end
+    best
+  end
+
   # calculate visible asteroids for each non-empty pixel
   # @return [Array<Array<Integer, NilClass>>] numbers of visible asteroids, or nil where empty
   def visible_asteroids
@@ -51,7 +92,7 @@ class AsteroidMap
         next if pixel.nil? || pixel.zero?
 
         # count only the unique keys (one / direction)
-        counters[x] = asteroid_directions(x, y).length # naive quadratic search
+        counters[x] = asteroid_directions(x, y).keys.length # naive quadratic search
       end
     end
   end
@@ -59,18 +100,18 @@ class AsteroidMap
   # finds unique directions to each other asteroids, given column x, row y
   # @param column_index [Integer] horizontal coordinate from left
   # @param row_index [Integer] vertical coordinate from top
-  # @return [Hash<String, Array>] direction vector => distances
+  # @return [Hash<String, Array>] direction => distances
   def asteroid_directions(column_index, row_index)
     directions = {}
     @rows.each.with_index do |row, y|
       row.each.with_index do |pixel, x|
         next if pixel.nil? || pixel.zero? || (x == column_index && y == row_index)
 
-        key, dist = compute_key_dist(column_index, x, row_index, y)
-        if directions[key].nil?
-          directions[key] = [dist] # first one in this direction
+        dir, dist = compute_direction_distance(column_index, x, row_index, y)
+        if directions[dir].nil?
+          directions[dir] = [dist] # first one in this direction
         else
-          directions[key] << dist # list each distance, even if behind each others
+          directions[dir] << dist # list each distance, even if behind each others
         end
       end
     end
@@ -79,18 +120,34 @@ class AsteroidMap
 
   private
 
-  # calculate some numbers
-  def compute_key_dist(x1, x2, y1, y2)
-    x_diff = x1 - x2
-    y_diff = y1 - y2
-    sq_dist = x_diff ** 2 + y_diff ** 2
+  # calculate some numbers: direction and squared distance
+  # @param xi1 [Integer] matrix row index of monitoring station
+  # @param xi2 [Integer] matrix row index of target
+  # @param yi1 [Integer] matrix column index of monitoring station
+  # @param yi2 [Integer] matrix column index of target
+  def compute_direction_distance(xi1, xi2, yi1, yi2)
+    x_diff = xi2 - xi1
+    y_diff = yi2 - yi1
+    sq_dist = x_diff**2 + y_diff**2
 
     denom = x_diff.gcd(y_diff)
     x_dir = x_diff / denom
     y_dir = y_diff / denom
 
-    key = "#{x_dir},#{y_dir}" # unique for each direction
-    [key, sq_dist]
+    dir = direction(x_dir, y_dir) # unique for each direction
+    [dir, { distance: sq_dist, x: x_diff, y: y_diff }]
+  end
+
+  # Calculates direction in radians with perverted matrix coordinates instead of Cartesian ones:
+  # (x_diff, y_diff) = (0, -1) points up at 0 rad
+  # (x_diff, y_diff) = (1, 0) points right at pi/2 rad
+  # (x_diff, y_diff) = (0, 1) points down at pi rad
+  # (x_diff, y_diff) = (-1, 0) points left at 3*pi/2 rad
+  # @param x_diff [Integer] difference in row indices
+  # @param y_diff [Integer] difference in column indices
+  # @return [Float] angle from top to target, clockwise, in (0, 2*PI)
+  def direction(x_diff, y_diff)
+    Math.atan2(0, -1) - Math.atan2(x_diff, y_diff)
   end
 
 end
@@ -99,18 +156,12 @@ if $PROGRAM_NAME == __FILE__
   # CLI for the 10th day
   am = AsteroidMap.new
   File.readlines(ARGV[0]).each { |line| am.add_row(line.chars) }
-  best = {}
-  all = am.visible_asteroids
-  all.each.with_index do |row, y|
-    row.each.with_index do |count, x|
-      next if count.nil?
+  # $stdout.puts am.to_s
+  monitoring_station = am.best_monitoring_asteroid
+  $stdout.puts monitoring_station.inspect
 
-      if best[:count].nil? || best[:count] < count
-        best[:count] = count
-        best[:coordinates] = [x, y]
-      end
-    end
+  point = monitoring_station[:coordinates]
+  am.lazer_sweep(point[0], point[1]).each.with_index do |asteroid, index|
+    $stdout.puts "#{index + 1}:#{asteroid.inspect}"
   end
-  $stdout.puts am.to_s
-  $stderr.puts best.inspect
 end
--- 2019/spec/asteroid_map_spec.rb
+++ 2019/spec/asteroid_map_spec.rb
@@ -42,6 +42,52 @@ RSpec.describe AsteroidMap do
     end
   end
 
+  context 'with the third example' do
+    before do
+      amap.add_row(['#', '.', '#', '.', '.', '.', '#', '.', '#', '.'])
+      amap.add_row(['.', '#', '#', '#', '.', '.', '.', '.', '#', '.'])
+      amap.add_row(['.', '#', '.', '.', '.', '.', '#', '.', '.', '.'])
+      amap.add_row(['#', '#', '.', '#', '.', '#', '.', '#', '.', '#'])
+      amap.add_row(['.', '.', '.', '.', '#', '.', '#', '.', '#', '.'])
+      amap.add_row(['.', '#', '#', '.', '.', '#', '#', '#', '.', '#'])
+      amap.add_row(['.', '.', '#', '.', '.', '.', '#', '#', '.', '.'])
+      amap.add_row(['.', '.', '#', '#', '.', '.', '.', '.', '#', '#'])
+      amap.add_row(['.', '.', '.', '.', '.', '.', '#', '.', '.', '.'])
+      amap.add_row(['.', '#', '#', '#', '#', '.', '#', '#', '#', '.'])
+    end
+
+    it 'contains one asteroid with max 35 other visible asteroids' do
+      expect(amap.visible_asteroids.flatten.compact.max).to eq(35)
+    end
+
+    it 'has 35 visible asteroids at 1,2' do
+      expect(amap.visible_asteroids[2][1]).to eq(35)
+    end
+  end
+
+  context 'with the fourth example' do
+    before do
+      amap.add_row(['.', '#', '.', '.', '#', '.', '.', '#', '#', '#'])
+      amap.add_row(['#', '#', '#', '#', '.', '#', '#', '#', '.', '#'])
+      amap.add_row(['.', '.', '.', '.', '#', '#', '#', '.', '#', '.'])
+      amap.add_row(['.', '.', '#', '#', '#', '.', '#', '#', '.', '#'])
+      amap.add_row(['#', '#', '.', '#', '#', '.', '#', '.', '#', '.'])
+      amap.add_row(['.', '.', '.', '.', '#', '#', '#', '.', '.', '#'])
+      amap.add_row(['.', '.', '#', '.', '#', '.', '.', '#', '.', '#'])
+      amap.add_row(['#', '.', '.', '#', '.', '#', '.', '#', '#', '#'])
+      amap.add_row(['.', '#', '#', '.', '.', '.', '#', '#', '.', '#'])
+      amap.add_row(['.', '.', '.', '.', '.', '#', '.', '#', '.', '.'])
+    end
+
+    it 'contains one asteroid with max 41 other visible asteroids' do
+      expect(amap.visible_asteroids.flatten.compact.max).to eq(41)
+    end
+
+    it 'has 41 visible asteroids at 6,3' do
+      expect(amap.visible_asteroids[3][6]).to eq(41)
+    end
+  end
+
   context 'with the fifth example' do
     before do
       amap.add_row(['.', '#', '.', '.', '#', '#', '.', '#', '#', '#', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#'])
@@ -72,6 +118,64 @@ RSpec.describe AsteroidMap do
 
     it 'has 210 visible asteroids at 11,13' do
       expect(amap.visible_asteroids[13][11]).to eq(210)
+    end
+
+    it 'has most visible asteroids at 11,13' do
+      expect(amap.best_monitoring_asteroid[:coordinates]).to contain_exactly(11, 13)
+    end
+
+    it 'the best monitoring asteroid has 210 other visible asteroids' do
+      expect(amap.best_monitoring_asteroid[:count]).to eq(210)
+    end
+
+    context 'station at 11,13' do
+      let :x { 11 }
+      let :y { 13 }
+      subject { amap.lazer_sweep(x, y) }
+
+      it 'will vaporize asteroid at 11,12 first' do
+        expect(subject.first).to contain_exactly(11, 12)
+      end
+
+      it 'will vaporize asteroid at 12,1 second' do
+        expect(subject[1]).to contain_exactly(12, 1)
+      end
+
+      it 'will vaporize asteroid at 12,2 third' do
+        expect(subject[2]).to contain_exactly(12, 2)
+      end
+
+      it 'will vaporize asteroid at 12,8 tenth' do
+        expect(subject[9]).to contain_exactly(12, 8)
+      end
+
+      it 'will vaporize asteroid at 16,0 20th' do
+        expect(subject[19]).to contain_exactly(16, 0)
+      end
+
+      it 'will vaporize asteroid at 16,9 50th' do
+        expect(subject[49]).to contain_exactly(16, 9)
+      end
+
+      it 'will vaporize asteroid at 10,16 100th' do
+        expect(subject[99]).to contain_exactly(10, 16)
+      end
+
+      it 'will vaporize asteroid at 9,6 199th' do
+        expect(subject[198]).to contain_exactly(9, 6)
+      end
+
+      it 'will vaporize asteroid at 8,2 200th' do
+        expect(subject[199]).to contain_exactly(8, 2)
+      end
+
+      it 'will vaporize asteroid at 10,9 201st' do
+        expect(subject[200]).to contain_exactly(10, 9)
+      end
+
+      it 'will vaporize asteroid at 11,1 last' do
+        expect(subject.last).to contain_exactly(11, 1)
+      end
     end
   end
 end
