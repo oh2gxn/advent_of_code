4dbdf785bbadd2920313f40f980473b44aeffee5
Janne Toivola <jammu81@iki.fi>
2020-01-14 21:36:07 +0200

    2019 day 15 part 2

--- 2019/repair_droid.rb
+++ 2019/repair_droid.rb
@@ -8,6 +8,7 @@
 # @author oh2gxn
 
 require 'csv'
+require 'time'
 require_relative 'int_code'
 
 # Remotely operated repair droid
@@ -16,11 +17,20 @@ class RepairDroid
   # wall tiles are negative
   WALL = -1
 
+  # input signals
+  NOT_MOVED = 0
+  MOVED = 1
+  MOVED_AND_FOUND_OXYGEN = 2
+
   # directions
-  NORTH = 1
-  SOUTH = 2
-  WEST = 3
-  EAST = 4
+  DIRECTIONS = { north: { signal: 1, row: -1, col:  0 },
+                 south: { signal: 2, row: 1, col: 0 },
+                 west: { signal: 3, row: 0, col: -1 },
+                 east: { signal: 4, row: 0, col: 1 } }.freeze
+
+  # stopping criteria for running
+  UNTIL_ALL_FOUND = 0
+  UNTIL_OXYGEN_FOUND = 2
 
   ##
   # Set up a RepairDroid to a remote space
@@ -31,20 +41,24 @@ class RepairDroid
     @area = Array.new(@area_size[0]) { Array.new(@area_size[1]) }
     @row = 2
     @col = 2
-    @area[@row][@col] = 0 # the 1st distance
-    @row_diff = 0
-    @col_diff = 1 # 1st direction = right
+    clear_distances
+    @direction = DIRECTIONS.keys.first # arbitrary direction
+    @promising_row = nil # bias towards some location
+    @promising_col = nil # bias towards some location
 
     # IO to the IntCode computer
+    @stop = nil # default
     @input, controller_output = IO.pipe
     controller_input, @output = IO.pipe
     @computer = IntCode.new(program, controller_input, controller_output, nil)
   end
 
   # Run two threads: the contoller and robot actuators
-  def run
-    robot = Thread.new { loop { break if react.nil? } }
-    controller = Thread.new { @computer.run(0) }
+  # @param stopping_criterion [Integer] RepairDroid::UNTIL_X
+  def run(stopping_criterion = UNTIL_ALL_FOUND)
+    @stop = stopping_criterion
+    robot = Thread.new { loop { break unless react } }
+    @controller ||= Thread.new { @computer.run(0) }
     robot.join # wait for robot to count final value
   end
 
@@ -70,9 +84,38 @@ class RepairDroid
     str.string
   end
 
-  # get current minimum Manhattan distance from starting point
-  def current_minimum_distance
+  # clears the distance values, sets current location to 0
+  def clear_distances
+    @area.each_with_index do |row, r|
+      row.each_with_index do |panel, c|
+        next if panel.nil? # still unknown
+
+        next if panel.negative? # keep walls
+
+        @area[r][c] = nil # panel inside, reset
+      end
+    end
+    @area[@row][@col] = 0
+  end
+
+  # get current Manhattan distance from starting point
+  def current_distance
     @area[@row][@col]
+  end
+
+  # get maximum Manhattan distance from starting point
+  def maximum_distance
+    max = 0
+    @area.each do |row|
+      row.each do |panel|
+        next if panel.nil? # still unknown
+
+        next if panel.negative? # walls
+
+        max = panel if panel > max
+      end
+    end
+    max
   end
 
   # Read some output from IntCode controller and provide more input
@@ -85,112 +128,141 @@ class RepairDroid
   private
 
   # finalizes movement
-  # @return [Integer, NilClass] painted color, or nil if quitting
+  # @return truthy unless quitting
   def sense
-    return nil if @input.eof?
+    return false if @input.eof?
 
     signal = @input.gets.chomp.to_i
-    new_tile = case signal
-               when 0
-                 update_aimed_tile
-               when 1, 2
-                 move
-                 update_current_tile
-               else
-                 raise(ArgumentError, "Unknown reply signal: #{signal}")
-               end
-    signal == 2 ? nil : new_tile
-  end
-
-  # turns the robot
-  def try_moving
-    # TODO: better algorithm
-    neighbor = WALL
-    until neighbor.nil? || !neighbor.negative?
-      signal = [NORTH, SOUTH, WEST, EAST].sample # random direction
-      case signal
-      when NORTH
-        north
-      when SOUTH
-        south
-      when WEST
-        west
-      when EAST
-        east
-      else
-        raise(ArgumentError, "Unknown movement signal: #{signal}")
-      end
-      neighbor = @area[@row + @row_diff][@col + @col_diff]
+    case signal
+    when NOT_MOVED
+      update_aimed_tile
+    when MOVED, MOVED_AND_FOUND_OXYGEN
+      move
+      update_current_tile
+    else
+      raise(ArgumentError, "Unknown reply signal: #{signal}")
     end
 
-    @output.puts(signal.to_s)
+    if ENV['DEBUG']
+      $stderr.print "\033c" # \033c reset terminal
+      $stderr.puts to_s
+      sleep 0.01
+    end
+
+    case @stop
+    when UNTIL_OXYGEN_FOUND
+      signal != MOVED_AND_FOUND_OXYGEN
+    when UNTIL_ALL_FOUND
+      has_holes?
+    else
+      false # quit after one round
+    end
   end
 
-  # turns north
-  def north
-    @row_diff = -1
-    @col_diff = 0
+  # check if there can be more reachable places
+  def has_holes?
+    # known hole, typically close
+    unless @promising_row.nil? || @promising_col.nil?
+      return true if @area[@promising_row][@promising_col].nil?
+    end
+
+    # find some distant hole, save it as a side effect
+    # FIXME: fails to find
+    @area.each_with_index do |row, r|
+      row.each_with_index do |panel, c|
+        next if panel.nil? # still unknown
+
+        next if panel.negative? # not inside
+
+        # r,c on the seen path: check for leaks
+        hole = neighbors_with_nil(r, c).first
+        unless hole.nil?
+          @promising_row = hole[0]
+          @promising_col = hole[1]
+          return true
+        end
+      end
+    end
+    false
   end
 
-  # turns south
-  def south
-    @row_diff = 1
-    @col_diff = 0
+  # bias candidate directions towards known holes
+  def directions_towards_promising_location
+    candidates = []
+    return candidates if @promising_row.nil? || @promising_col.nil?
+
+    DIRECTIONS.each do |key, value|
+      diff = [@promising_row - @row, @promising_col - @col]
+      candidates << key if diff[0].positive? && value[:row].positive?
+      candidates << key if diff[0].negative? && value[:row].negative?
+      candidates << key if diff[1].positive? && value[:col].positive?
+      candidates << key if diff[1].negative? && value[:col].negative?
+    end
+    candidates
   end
 
-  # turns west
-  def west
-    @row_diff = 0
-    @col_diff = -1
-  end
-
-  # turns east
-  def east
-    @row_diff = 0
-    @col_diff = 1
+  # tries to move the droid
+  def try_moving
+    # TODO: better algorithm than biased random walk?
+    neighbor = WALL # imagine hitting a wall
+    direction = @direction
+    until neighbor.nil? || !neighbor.negative?
+      candidates = DIRECTIONS.keys # consider any direction
+                     .concat([@direction]) # prefer current direction
+                     .concat(directions_towards_promising_location) # prefer known hole
+      # puts candidates.inspect
+      direction = candidates.sample # random direction
+      row = @row + DIRECTIONS[direction][:row]
+      col = @col + DIRECTIONS[direction][:col]
+      neighbor = @area[row][col]
+    end
+    @direction = direction
+    @output.puts(DIRECTIONS[@direction][:signal].to_s)
   end
 
   # moves the current position of droid
   def move
-    @row += @row_diff
+    @row += DIRECTIONS[@direction][:row]
     prepend_height unless @row.positive?
     append_height if @row >= @area_size[0] - 1
 
-    @col += @col_diff
+    @col += DIRECTIONS[@direction][:col]
     prepend_width unless @col.positive?
     append_width if @col >= @area_size[1] - 1
   end
 
   # discover some more area at the top
   def prepend_height
-    diff = @area_size[0]
+    diff = 1 # @area_size[0] # doubling strategy?
     new_area = Array.new(diff) { Array.new(@area_size[1]) }
     new_area += @area
     @area = new_area
     @area_size[0] += diff
+    @promising_row += diff unless @promising_row.nil?
     @row += diff
   end
 
   # discover some more area at the bottom
   def append_height
-    diff = @area_size[0]
+    diff = 1 # @area_size[0]
     @area += Array.new(diff) { Array.new(@area_size[1]) }
     @area_size[0] += diff
   end
 
   # discover some more area at left
   def prepend_width
-    diff = @area_size[1]
+    diff = 1 # @area_size[1]
     @area.each.with_index do |row, i|
       @area[i] = Array.new(diff) + row
     end
     @area_size[1] += diff
+    @promising_col += diff unless @promising_col.nil?
     @col += diff
   end
 
   # discover some more area at right
   def append_width
-    diff = @area_size[1]
+    diff = 1 # @area_size[1]
     @area.each.with_index do |row, i|
       @area[i] = row + Array.new(diff)
     end
@@ -199,22 +271,59 @@ class RepairDroid
 
   # update a wall tile
   def update_aimed_tile
-    @area[@row + @row_diff][@col + @col_diff] = WALL
+    row = @row + DIRECTIONS[@direction][:row]
+    col = @col + DIRECTIONS[@direction][:col]
+    if @promising_row == row && @promising_row == col
+      # patched a hole in the wall, find another one
+      @promising_row = nil
+      @promising_col = nil
+    end
+    @area[row][col] = WALL
   end
 
   # update minimum distance to current position
   def update_current_tile
-    neighbors = [@area[@row - 1][@col],
-                 @area[@row + 1][@col],
-                 @area[@row][@col - 1],
-                 @area[@row][@col + 1]]
-    candidate = neighbors.reject { |d| d.nil? || d.negative? }.min + 1
+    if @promising_row == @row && @promising_row == @col
+      # moved onto the promising position, find new one
+      hole = neighbors_with_nil(@row, @col).sample
+      if hole.nil?
+        # need to find a more distant one, in has_holes?
+        @promising_row = nil
+        @promising_col = nil
+      else
+        # found a nearby hole
+        @promising_row = hole[0]
+        @promising_col = hole[1]
+      end
+    end
+
+    # update current minimum distance
+    candidate = neighbors_visited(@row, @col).map { |n| @area[n[0]][n[1]] }.min + 1
     current = @area[@row][@col]
     if current.nil? || current > candidate
+      # found a better route
       @area[@row][@col] = candidate
     else
       current
     end
+  end
+
+  # locations next to given one
+  def neighbors(row, col)
+    [[row - 1, col],
+     [row + 1, col],
+     [row, col - 1],
+     [row, col + 1]]
+  end
+
+  # check if a location would "leak" to unknown area
+  def neighbors_with_nil(row, col)
+    neighbors(row, col).select { |n| @area[n[0]][n[1]].nil? }
+  end
+
+  # neighboring locations which have been seen
+  def neighbors_visited(row, col)
+    neighbors(row, col).reject { |n| @area[n[0]][n[1]].nil? || @area[n[0]][n[1]].negative? }
   end
 
 end
@@ -229,8 +338,19 @@ if $PROGRAM_NAME == __FILE__
   program = CSV.read(ARGV[0]).first
 
   # part 1
+  t0 = Time.now.utc
   droid = RepairDroid.new(program)
-  droid.run
-  $stdout.puts droid.to_s
-  $stdout.puts "found oxygen system #{droid.current_minimum_distance} steps from starting point"
+  droid.run(RepairDroid::UNTIL_OXYGEN_FOUND)
+  $stderr.puts droid.to_s
+  t1 = Time.now.utc
+  $stdout.puts "found oxygen system #{droid.current_distance} steps from starting point"
+  $stdout.puts "took #{t1 - t0} s"
+
+  # part 2
+  droid.clear_distances # new starting point == oxygen system, count distances from oxygen
+  droid.run(RepairDroid::UNTIL_ALL_FOUND) # continues running the same Intcode
+  t2 = Time.now.utc
+  $stderr.puts droid.to_s
+  $stdout.puts "found everything #{droid.maximum_distance} steps from starting point"
+  $stdout.puts "took #{t2 - t1} s"
 end
